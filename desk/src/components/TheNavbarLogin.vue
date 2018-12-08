6e71a3a5aeec93ec1094fbc6aba4c2b1fb65d4c4
Faris Ansari <netchamp.faris@gmail.com>
2018-12-09 00:28:55 +0530

    Move session info to Vuex

--- desk/src/components/TheNavbarLogin.vue
+++ desk/src/components/TheNavbarLogin.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="font-semibold">
-        <a class="no-underline text-grey-dark hover:text-grey-darker" href="#" role="button" v-if="!userInfo"
+        <a class="no-underline text-grey-dark hover:text-grey-darker" href="#" role="button" v-if="!session"
             @click="login('faris@erpnext.com', 'qwe')">Login</a>
         <div class="flex items-center" v-else>
-            <UserAvatar class="mr-2" :user="userInfo.email" />
+            <UserAvatar class="mr-2" :user="session.email" />
             <a class="no-underline text-grey-dark hover:text-grey-darker" href="#" role="button">
-                {{ userInfo.first_name }}
+                {{ session.first_name }}
             </a>
         </div>
     </div>
@@ -13,21 +13,17 @@
 <script>
 export default {
     name: 'TheNavbarLogin',
-    data() {
-        return {
-            userInfo: null
+    computed: {
+        session() {
+            return this.$store.state.session
         }
     },
     created() {
-        this.fetchUserInfo()
+        this.$store.dispatch('fetchUserInfo')
     },
     methods: {
         async login(email='Administrator', password='qwe') {
-            await this.$call('login', { usr: email, pwd: password })
-            this.fetchUserInfo()
-        },
-        async fetchUserInfo() {
-            this.userInfo = await this.$call('frappe.utils.user.get_user_info')
+            this.$store.dispatch('login', { email, password })
         }
     }
 }
--- desk/src/store/index.js
+++ desk/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import frappe from '../frappe'
 import Meta from './Meta'
 import Desktop from '../pages/Desktop.store'
 import List from '../pages/List.store'
@@ -9,6 +10,21 @@ Vue.use(Vuex);
 const store = new Vuex.Store({
     state: {
         session: null
+    },
+    mutations: {
+        setSession(state, { userInfo }) {
+            state.session = userInfo
+        }
+    },
+    actions: {
+        async login({ dispatch }, { email, password }) {
+            await frappe.call('login', { usr: email, pwd: password })
+            await dispatch('fetchUserInfo')
+        },
+        async fetchUserInfo({ commit }) {
+            const userInfo = await frappe.call('frappe.utils.user.get_user_info')
+            commit('setSession', { userInfo })
+        }
     },
     modules: {
         Meta,
