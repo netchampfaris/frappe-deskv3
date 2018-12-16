c0dd8269a9b21c7a9fa7509f57cdb96fa5d1ee7c
Faris Ansari <netchamp.faris@gmail.com>
2018-12-16 20:32:12 +0530

    Add PageHeader settings

--- desk/src/pages/List.vue
+++ desk/src/pages/List.vue
@@ -1,17 +1,18 @@
 <template>
-    <div class="list" v-if="meta">
-        <div class="container">
-            <div class="flex">
-                <div class="w-1/6 border-r">
-                    <ListSidebar :doctype="doctype" />
-                </div>
-                <div class="w-5/6 border-r">
-                    <FilterArea :doctype="doctype" />
-                    <ListMain class="border-t" :doctype="doctype" />
-                </div>
-            </div>
+  <div class="list" v-if="meta">
+    <ThePageHeader v-bind="pageHeaderSettings"/>
+    <div class="container">
+      <div class="flex">
+        <div class="w-1/6 border-r">
+          <ListSidebar :doctype="doctype"/>
         </div>
+        <div class="w-5/6 border-r">
+          <FilterArea :doctype="doctype"/>
+          <ListMain class="border-t" :doctype="doctype"/>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 import ListSidebar from './ListSidebar'
@@ -19,30 +20,39 @@ import ListMain from './ListMain'
 import FilterArea from '../components/FilterArea'
 
 export default {
-    name: 'List',
-    props: ['doctype'],
-    components: {
-        ListSidebar,
-        ListMain,
-        FilterArea
+  name: 'List',
+  props: ['doctype'],
+  components: {
+    ListSidebar,
+    ListMain,
+    FilterArea,
+  },
+  created() {
+    this.$store.dispatch('Meta/fetchMeta', { doctype: this.doctype })
+    this.$store.commit('CurrentPage/setPageHeaderSettings', {
+      title: this.doctype,
+      primaryActionLabel: 'New',
+      primaryAction: console.log,
+      secondaryActionLabel: 'Refresh',
+      secondaryAction: console.log,
+      menuItems: ['Test'],
+    })
+  },
+  computed: {
+    meta() {
+      return this.$store.getters['Meta/getMeta'](this.doctype)
     },
-    created() {
-        this.$store.dispatch('Meta/fetchMeta', { doctype: this.doctype })
-        this.$store.commit('CurrentPage/setPageHeaderSettings', {
-            title: this.doctype,
-            primaryActionLabel: 'New',
-            primaryAction: console.log,
-            secondaryActionLabel: 'Refresh',
-            secondaryAction: console.log,
-            menuItems: [
-                'Test'
-            ]
-        })
+    pageHeaderSettings() {
+      return {
+        showPageHeader: true,
+        title: this.doctype,
+        primaryActionLabel: 'New',
+        primaryAction: console.log,
+        secondaryActionLabel: 'Refresh',
+        secondaryAction: console.log,
+        menuItems: ['Test'],
+      }
     },
-    computed: {
-        meta() {
-            return this.$store.getters['Meta/getMeta'](this.doctype)
-        }
-    }
+  },
 }
 </script>
