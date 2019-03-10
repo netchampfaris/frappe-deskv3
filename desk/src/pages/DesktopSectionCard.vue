89433a8ed2b9c1541db2b32342c96d492cbf53b8
scmmishra <scm.mymail@gmail.com>
2019-03-10 20:00:16 +0530

    feat: Added links to cards

--- desk/src/pages/DesktopSectionCard.vue
+++ desk/src/pages/DesktopSectionCard.vue
@@ -3,15 +3,18 @@
     class="py-6 px-4 rounded bg-white border hover:shadow cursor-pointer focus:shadow-none focus:border-blue focus:shadow-inner outline-none flex justify-between items-center"
     tabindex="0"
   >
-    <div class="flex items-center">
-      <FeatherIcon class="w-12 text-muted" :name="item.feather" :size="24"/>
-      <h3 class="text-dark font-normal">{{ item.label }}</h3>
-    </div>
+    <a
+      class="flex items-center no-underline focus:underline focus:outline-none hover:underline"
+      :href="link"
+    >
+      <FeatherIcon class="w-12 text-muted" :name="item.feather" :size="24" />
+      <h3 class="text-dark text-xl font-medium">{{ item.label }}</h3>
+    </a>
     <Dropdown v-if="item.links" :items="item.links" class="text-sm">
       <button
-        class="flex hover:bg-grey-lighter p-1 rounded-full focus:outline-none focus:border-blue"
+        class="flex hover:bg-grey-lighter focus:bg-grey-lighter p-1 rounded-full focus:outline-none focus:border-blue"
       >
-        <FeatherIcon name="chevron-down" class="self-center"/>
+        <FeatherIcon name="chevron-down" class="self-center" />
       </button>
     </Dropdown>
   </div>
@@ -20,5 +23,12 @@
 export default {
   name: 'DesktopSectionCard',
   props: ['item'],
+  computed: {
+    link() {
+      return this.item.type === 'module'
+        ? '#modules/' + this.item.label
+        : this.item.link
+    },
+  },
 }
 </script>
