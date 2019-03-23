da5163814d84db2607be31605b0ea59c5a443df6
Faris Ansari <netchamp.faris@gmail.com>
2019-03-23 13:13:03 +0530

    fix: Indicator dimensions

--- desk/src/components/Indicator.vue
+++ desk/src/components/Indicator.vue
@@ -1,8 +1,8 @@
 <template>
-    <span
-        class="inline-block w-3 h-3 rounded-circle mr-2"
-        :class="[color ? 'bg-' + color : 'bg-blue']">
-    </span>
+  <span
+    class="inline-block rounded-circle mr-2 indicator"
+    :class="[color ? 'bg-' + color : 'bg-blue']"
+  ></span>
 </template>
 <script>
 export default {
@@ -10,3 +10,9 @@ export default {
   props: ['color'],
 }
 </script>
+<style>
+.indicator {
+  height: 8px;
+  width: 8px;
+}
+</style>
