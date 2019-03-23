8eb5eb5177aee8d933a010e921e3255e0ce03374
Faris Ansari <netchamp.faris@gmail.com>
2019-03-23 13:20:29 +0530

    feat: Add StatusIndicator

--- desk/src/pages/ListMainRow.vue
+++ desk/src/pages/ListMainRow.vue
@@ -1,23 +1,35 @@
 <template>
-    <div class="px-4 py-3 hover:bg-grey-lightest cursor-pointer border-b">
-        <div class="flex">
-            <div class="w-4/5 flex items-center">
-                <div class="w-2/5">
-                    {{ doc[titleField] | stripHTML }}
-                </div>
-                <div class="w-1/5" v-for="fieldname in otherFields" :key="fieldname">
-                    {{ doc[fieldname] | stripHTML }}
-                </div>
-            </div>
-            <div class="w-1/5 flex justify-end items-center">
-                <UserAvatar :user="assignedTo" class="mr-3" />
-                <div class="inline-flex items-center" :class="[ doc._comment_count ? 'text-grey-dark' : 'text-grey-light']">
-                    <octicon name="comment-discussion" class="mr-2" />
-                    <span>{{ doc._comment_count }}</span>
-                </div>
-            </div>
+  <div class="px-4 py-3 hover:bg-grey-lightest cursor-pointer border-b">
+    <div class="flex">
+      <div class="w-4/5 flex items-center">
+        <div class="w-2/5">
+          {{ doc[titleField] | stripHTML }}
         </div>
+        <div
+          class="w-1/5 text-muted"
+          v-for="fieldname in otherFields"
+          :key="fieldname"
+        >
+          <StatusIndicator
+            v-if="fieldname === 'status'"
+            :doctype="doctype"
+            :name="doc.name"
+          />
+          <span v-else>{{ doc[fieldname] | stripHTML }}</span>
+        </div>
+      </div>
+      <div class="w-1/5 flex justify-end items-center">
+        <UserAvatar :user="assignedTo" class="mr-3" />
+        <div
+          class="inline-flex items-center"
+          :class="[doc._comment_count ? 'text-grey-dark' : 'text-grey-light']"
+        >
+          <FeatherIcon name="message-square" class="mr-2" />
+          <span>{{ doc._comment_count }}</span>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 export default {
